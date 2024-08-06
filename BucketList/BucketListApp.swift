d7d6161c6fc24cdc1fbbd7a99a6faf2d2b14f7f7
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-06 19:17:01 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
